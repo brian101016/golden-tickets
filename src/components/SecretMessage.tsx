1890e36399ad275c1defb0c666486d904876f420
Brian Woolfolk <brianwoolfolk@hotmail.com>
2024-10-01 00:10:13 -0700

    Audio controls

--- src/components/SecretMessage.tsx
+++ src/components/SecretMessage.tsx
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components";
 import { parseCSS } from "scripts/FunctionsBundle";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import BG_MUSIC from "@theme/SFX/bg/bg-piano.mp3";
 
 // #region ##################################################################################### PROPS
 type _Base = import("@utils/ClassTypes")._Base;
@@ -12,11 +13,52 @@ type SecretMessageProps = {} & _Base;
 const _SecretMessage = (props: SecretMessageProps) => {
   const [open, setOpen] = useState(true);
   const [enableButton, setEnableButton] = useState(false);
+  const [bgMusic] = useState(new Audio(BG_MUSIC));
+  const ref = useRef<HTMLDivElement>(null);
+
+  function handleMusic() {
+    if (ref.current === null) return;
+
+    let option = 0;
+    // IF IS MUTED, WILL TURN ON
+    if (ref.current.classList.contains("bi-volume-mute-fill")) {
+      ref.current.classList.remove("bi-volume-mute-fill");
+      ref.current.classList.add("bi-volume-up-fill");
+      option = 3;
+    } else if (ref.current.classList.contains("bi-volume-up-fill")) {
+      ref.current.classList.remove("bi-volume-up-fill");
+      ref.current.classList.add("bi-volume-down-fill");
+      option = 2;
+    } else if (ref.current.classList.contains("bi-volume-down-fill")) {
+      ref.current.classList.remove("bi-volume-down-fill");
+      ref.current.classList.add("bi-volume-off-fill");
+      option = 1;
+    } else {
+      ref.current.classList.remove("bi-volume-off-fill");
+      ref.current.classList.add("bi-volume-mute-fill");
+    }
+
+    if (bgMusic.paused && option > 0) {
+      bgMusic.play();
+    } else if (!bgMusic.paused && option <= 0) {
+      bgMusic.pause();
+    }
+
+    bgMusic.loop = true;
+    bgMusic.volume = option / 3;
+  }
 
   function handleClose() {
+    handleMusic();
     setOpen(false);
     setEnableButton(false);
   }
+
+  useEffect(() => {
+    return () => {
+      if (!bgMusic.paused) bgMusic.pause();
+    };
+  }, [bgMusic]);
 
   useEffect(() => {
     if (open) {
@@ -29,44 +71,49 @@ const _SecretMessage = (props: SecretMessageProps) => {
 
   // ---------------------------------------------------------------------- RETURN
   return (
-    <div
-      className={props.className + " modal-container " + (open ? "" : "hidden")}
-    >
-      <div className="modal">
-        <h3 className="secret-title">
-          ¡Shhh! <br />
-          <span className="to-shrink">¡Es una sorpresa!</span>
-        </h3>
+    <>
+      <div
+        className={
+          props.className + " modal-container " + (open ? "" : "hidden")
+        }
+      >
+        <div className="modal">
+          <h3 className="secret-title">
+            ¡Shhh! <br />
+            <span className="to-shrink">¡Es una sorpresa!</span>
+          </h3>
 
-        <p className="secret-body">
-          <span>
-            Esta invitación es estrictamente confidencial. <br />
-            <b>¡No reveles nada a Yolanda & Javier!</b>
-          </span>
+          <p className="secret-body">
+            <span>
+              Esta invitación es estrictamente confidencial. <br />
+              <b>¡No reveles nada a Yolanda & Javier!</b>
+            </span>
 
-          <span>
-            Tu discreción es fundamental para que esta sorpresa sea inolvidable.
-          </span>
+            <span>
+              Tu discreción es fundamental para que esta sorpresa sea
+              inolvidable.
+            </span>
 
-          <span>¡Muchas gracias por tu colaboración!</span>
+            <span>¡Muchas gracias por tu colaboración!</span>
+          </p>
 
-          <iframe
-            title="bgmusic"
-            src="https://freesound.org/embed/sound/iframe/744876/simple/small/"
-            width="375"
-            height="30"
-          ></iframe>
-        </p>
-
-        <button
-          className="secret-confirm input important success"
-          disabled={!enableButton}
-          onClick={handleClose}
-        >
-          Guardaré el secreto
-        </button>
+          <button
+            className="secret-confirm input important success"
+            disabled={!enableButton}
+            onClick={handleClose}
+          >
+            Guardaré el secreto
+          </button>
+        </div>
       </div>
-    </div>
+
+      <div
+        className="bi-volume-mute-fill bg-music"
+        hidden={open}
+        ref={ref}
+        onClick={handleMusic}
+      />
+    </>
   );
 };
 // #endregion
